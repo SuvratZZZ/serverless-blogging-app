ce1f64189ea3462498078e6962360f6dce93261d
SuvratZZZ <singhsubrat35@gmail.com>
2025-01-21 18:15:09 +0530

    managed blog send route

--- frontend/src/components/Inup.tsx
+++ frontend/src/components/Inup.tsx
@@ -1,9 +1,19 @@
-function Inup( {label , placeholder , refr , wid}: 
-    {label : string , placeholder : string , refr? : any , wid?:string }) {
+function Inup( {label , placeholder , refr , wid , type}: 
+    {label : string , placeholder : string , refr? : any , wid?:string , type?: string}) {
     return (
-        <div className={`h-${wid}`}>
-            <h2>{label} : </h2>
-            <input className={`p-1 w-full border-2 rounded-md`} placeholder={placeholder} ref={refr}></input>
+        <div>
+        {
+            (type != "tex" ) ?
+            (<div className={`h-${wid}`}>
+                <h2>{label} : </h2>
+                <input className={`p-1 h-${wid} w-full border-2 rounded-md`} placeholder={placeholder} ref={refr}></input>
+            </div>):(
+                <div className={`h-${wid}`}>
+                <h2>{label} : </h2>
+                <textarea className={`p-1 h-${wid} w-full border-2 rounded-md`} placeholder={placeholder} ref={refr}></textarea>
+            </div>
+            )
+        }
         </div>
     );
 }
--- frontend/src/components/Postform.tsx
+++ frontend/src/components/Postform.tsx
@@ -2,26 +2,51 @@ import { useRef, useState } from "react";
 import Inup from "./Inup";
 
 function Postform() {
-    // const postref=useRef<{HTMLInputElement,HTMLInputElement}>(null);
+    const titleref=useRef<HTMLInputElement>();
+    const contentref=useRef<HTMLInputElement>();
+
+    const [sendpo,setsendpo] = useState<{}>({});
+    const [ale,setale] = useState<string>();
     const [postbox,setpostbox] = useState<boolean>(true);
     const togglePostbox=()=>{
         setpostbox(!postbox)
+    }
+    const sendPostobj = ()=>{
+        if(contentref.current?.value === undefined || titleref.current?.value === undefined){
+            setale('*enter valid content');
+            return;
+        }
+        else if(contentref.current?.value.length < 10 || contentref.current?.value.length === 0 ){
+            setale('*enter content of valid length');
+            return;
+        }
+        setsendpo({
+            "title"     : titleref.current?.value,
+            "content"   : contentref.current?.value 
+        })
+        setpostbox(true);
     }
     return (
         <div>
         {(postbox==false)?
             (   
                 <div className="fixed  bottom-0 right-0 m-4 p-5 h-2/3 w-2/3 bg-white shadow-lg rounded-lg border border-gray-200 flex flex-col">
+                        <div onClick={togglePostbox} className=" pr-1 full cursor-pointer z-15 font-bold text-right  " >X</div>
                         <div className="w-full flex">
-                            <h3 className="w-5/6 text-center">Create A Post</h3>
-                            <button onClick={togglePostbox} className=" pr-0 w-1/6 z-15 font-bold " >X</button>
+                            <h3 className="w-full text-center">Create A Post</h3>
                         </div>
-                        <Inup label="Title" placeholder="Enter nice title" refr={null} />
-                        <Inup label="Content" placeholder="Enter nice content" />
+                        
+                        <div className="w-full animate-pulse text-center text-red-600 text-xs font-thin "
+                        >{ale}</div>
+                        <Inup label="Title" placeholder="Enter nice title" refr={titleref} />
+                        <Inup label="Content" placeholder="Enter nice content" refr={contentref} type="tex" wid="50" />
+                        <div>
+                        <button onClick={sendPostobj} className=" fixed  bottom-0 right-0 pl-5 pr-5 pt-1 pb-1 bg-white shadow-lg rounded-lg border border-gray-200 m-8 z-15" >post blog</button>
+                        </div>
                 </div>
             ):(
                 <div>
-                <button onClick={togglePostbox} className=" fixed  bottom-0 right-0 m-4 z-15" >create blog</button>
+                <button onClick={togglePostbox} className=" fixed  bottom-0 right-0 p-5 bg-white shadow-lg rounded-lg border border-gray-200 m-4 z-15" >create blog</button>
                 </div>
             )}
         </div>
