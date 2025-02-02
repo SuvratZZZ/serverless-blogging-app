adfad8a39627b92fc683fbc05a5950b98c18f3ad
Subrat Singh <97746920+SuvratZZZ@users.noreply.github.com>
2025-02-02 18:09:35 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server:{
-    port: 8000,
     proxy: {
       // Ensure the path starts with a forward slash
       '/api': {
